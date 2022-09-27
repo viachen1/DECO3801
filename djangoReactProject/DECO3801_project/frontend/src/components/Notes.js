ae31174155901a0003e89f5b76c7769b89aa572a
kmindc <112996381+kmindc@users.noreply.github.com>
2022-09-27 16:06:06 +1000

    Add files via upload

---
+++ djangoReactProject/DECO3801_project/frontend/src/components/Notes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Notes(props) {
+  return (
+
+    <h1> This is our notes page</h1>
+
+  );
+}
+
+Notes.propTypes = {
+  window: PropTypes.func,
+};
+
+export default Notes;
---
+++ djangoReactProject/DECO3801_project/frontend/src/components/Sessions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Sessions(props) {
+  return (
+
+    <h1> This is our sessions page</h1>
+
+  );
+}
+
+Sessions.propTypes = {
+  window: PropTypes.func,
+};
+
+export default Sessions;
